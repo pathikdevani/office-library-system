cfb4e8c801aefd1428a9542ec273dff5b156bece
Sahil Dev <sahil.dev@imanage.com>
2019-09-17 20:06:45 +0530

    Adding admin and user page

---
+++ web/src/pages/AdminPage.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const AdminPage = (props) => {
+  const {} = props;
+  return (
+    <Fragment>Admin</Fragment>
+  );
+}
+
+export default AdminPage;
---
+++ web/src/pages/UserPage.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const UserPage = (props) => {
+  const {} = props;
+  return (
+    <Fragment>User</Fragment>
+  );
+}
+
+export default UserPage;
